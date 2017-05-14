b3397ecf9e74fd2a71e261f6e43b3bad3356d1c5
Luke Harrison <luke488488@gmail.com>
2017-05-14 15:49:44 -0600

    a few api calls

--- public/assets/js/userdisplay.js
+++ public/assets/js/userdisplay.js
@@ -1,22 +1,33 @@
-
+var uservar;
 $( document ).ready(function() {
     $(".Btasks").hide();
     $(".list").hide();
+
 });
+$(document).on("click", "#newTopic",function(){
+	
+	});
 $(document).on("click", ".btn",function(){
 	var sid = this.id;
 	var p = sid.search("_");
+	console.log("detected a click");
 	if(p > 9){
 		var use = sid.substr(p,sid.length);
 		use = "section-steplist"+use;
 	}else if(p > 1){
+	console.log("detected task click");
+		var dispArr = [];
 		var use = sid.substr(p,sid.length);
-		use = "section-task"+use;
+		for (var i = 0; i < uservar.topics.tasks.length; i++) {
+			if(uservar.topics.tasks[i].id === use){
+				console.log("got to append line");
+				$(".modal-header").append("<h5>"+uservar.topics.tasks[i].step_lists.list_title+"</h5>");
+			}
+		}
 	}
 	
 	console.log(use);
 	console.log(p);
-	$("#"+use).show();
 
 });
 $(document).on("click", ".hide",function(){
